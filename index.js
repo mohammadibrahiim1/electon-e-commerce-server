4c977479519a0f122d52cb70cc53c8f97c297bab
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-01-24 15:11:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// user-admin: user-admin
+// user-password: QvuYfoPkal4fSmbm
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("yes working!");
+});
+
+// data-base connection
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://user-admin:QvuYfoPkal4fSmbm@cluster0.wuwpwwx.mongodb.net/?retryWrites=true&w=majority";
+  console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+async function run() {
+    const productsCollection = client.db("electon-e-commerce").collection("all-products")
+
+    try{
+
+        // get all products
+
+        app.get( "/allproducts" , async(req, res)=> {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const allproducts = await cursor.toArray();
+            res.send(allproducts);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch((error)=> console.log(error));
+
+
+
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
+module.exports = app;
